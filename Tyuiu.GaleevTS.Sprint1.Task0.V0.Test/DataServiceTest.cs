51c16679da6a0a7825cd208b0ddf142bacc16f16
Тимур Галеев <timur.galeev.05@yandex.ru>
2023-09-20 22:22:00 +0500

    Add project files.

---
+++ Tyuiu.GaleevTS.Sprint1.Task0.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.GaleevTS.Sprint1.Task0.V0.Lib;
+
+namespace Tyuiu.GaleevTS.Sprint1.Task0.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(22, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.GaleevTS.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GaleevTS.Sprint1.Task0.V0.Lib;
+
+namespace Tyuiu.GaleevTS.Sprint1.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт №0 | Выполнил: Галеев Т. С. | ИИПб-23-3";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                              *");
+            Console.WriteLine("* Задания #0                                                               *");
+            Console.WriteLine("* Вариант #27                                                              *");
+            Console.WriteLine("* Выполнил: Галеев Тимур Серикович | ИИПб-23-3                             *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать консольную программу, которая вычисляет выражение 5 * 2 + 4 * 3 *");
+            Console.WriteLine("* и печатает результат на экране                                           *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*  5 * 2 + 4 * 3                                                           *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadKey();
+        }
+    }
+}
