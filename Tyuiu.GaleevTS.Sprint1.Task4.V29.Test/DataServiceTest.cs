90707c480e3e37ea5dc4192896d49dd864316244
Тимур Галеев <timur.galeev.05@yandex.ru>
2023-09-25 23:54:28 +0500

    Task4

---
+++ Tyuiu.GaleevTS.Sprint1.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.GaleevTS.Sprint1.Task4.V29.Lib;
+
+namespace Tyuiu.GaleevTS.Sprint1.Task4.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double x = 4;
+            double y = 1;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(0.167, res);
+
+        }
+    }
+}
