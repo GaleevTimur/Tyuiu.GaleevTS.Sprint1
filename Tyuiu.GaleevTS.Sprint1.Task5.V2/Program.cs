ee13c4f286c4d2d5738a53f53ce12ac6d7759493
Тимур Галеев <timur.galeev.05@yandex.ru>
2023-09-27 21:29:36 +0500

    Task5

---
+++ Tyuiu.GaleevTS.Sprint1.Task5.V2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GaleevTS.Sprint1.Task5.V2.Lib;
+
+namespace Tyuiu.GaleevTS.Sprint1.Task5.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт №1 | Выполнил: Галеев Т. С. | ИИПб-23-3";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                         *");
+            Console.WriteLine("* Задания #5                                                               *");
+            Console.WriteLine("* Вариант #2                                                               *");
+            Console.WriteLine("* Выполнил: Галеев Тимур Серикович | ИИПб-23-3                             *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,  *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране                  *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                          *");
+            Console.WriteLine("****************************************************************************");
+
+            double temp;
+            Console.WriteLine("Введите значение градусов в Фаренгейтах: ");
+            temp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            int tempT = Convert.ToInt32(ds.FahrenheitToСelsius(temp));
+            Console.WriteLine("Температура в градусах: " + tempT);
+
+            Console.ReadKey();
+        }
+    }
+}
